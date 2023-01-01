0a63a8d89fb4ff0c4efa9fc2e96deda189bbc6c4
Brook Nash <brooqnash@gmail.com>
2023-01-01 19:49:23 +0000

    ADD: Index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      Black: "#000000",
+      White: "#FFFFFF",
+      Grey: "#AAAAAA",
+      DarkGrey: "#AAAAAA15",
+      Transparent: "#00000000",
+      Red: "#CD5C5C",
+      Green: "#0B9E8A",
+      Blue: "#76B7C7",
+    },
   },
-  plugins: [],
 };
